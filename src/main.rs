fd1055eb7a420553ba84cb0c3ceb5b216a68a7c5
CCStudios <50129568+CodeCrashStudios@users.noreply.github.com>
2024-04-04 00:26:08 -0400

    Basic types and data store added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::collections::HashMap;
+
+enum Types {
+    Text(String),
+    List(Vec<Option<Types>>)
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut data_store: HashMap<String, Types> = HashMap::new();
+
+    data_store.insert(String::from("Test"), Types::Text(String::from("This is some data.")));
+
+    let vec = vec![Option::Some(Types::Text(String::from("This"))), Option::Some(Types::Text(String::from("Is"))), Option::Some(Types::Text(String::from("Data")))];
+
+    data_store.insert(String::from("ListTest"), Types::List(vec));
+
+    print!("Test: ");
+    print_data(data_store.get(&String::from("Test")));
+    println!();
+    print!("ListTest: ");
+    print_data(data_store.get(&String::from("ListTest")));
+    println!();
+    
+}
+
+
+fn print_data(data: Option<&Types>){
+    match data {
+        Some(i) => {
+            match i {
+                Types::Text(out) => {
+                    print!("\"{}\"", out);
+                },
+                Types::List(out) => {
+                    print!("{{ ");
+                    for i in 0..out.len(){
+                        print_data(out[i].as_ref());
+                        if i != out.len() - 1 {
+                            print!(", ");
+                        }
+                    }
+                    print!(" }}");
+                }
+            }
+        },
+        None =>{
+            println!("NULL");
+        }
+    }
 }
