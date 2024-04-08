cf13162888af31b7bdd07424a9b333c492584c76
CCStudios <50129568+CodeCrashStudios@users.noreply.github.com>
2024-04-08 00:07:52 -0400

    Working to make types closer to Redis

--- src/command_parser/command_parser.rs
+++ src/command_parser/command_parser.rs
@@ -2,49 +2,75 @@ use crate::datastore::DataStore;
 use crate::types;
 use crate::types::Types;
 
-
 /// Parses the command that is given and applies it to the given DataStore.
-pub fn parse(mut raw: &str, store: &mut DataStore){
-
+pub fn parse(mut raw: &str, store: &mut DataStore) {
     raw = raw.trim();
 
     // Removes trailing ';' if present since its not needed.
     if raw.ends_with(';') {
-        raw = &raw[0..raw.len()-1];
+        raw = &raw[0..raw.len() - 1];
     }
 
-    for line in raw.split(';'){
+    for line in raw.split(';') {
         run_command(line, store);
     }
 }
 
 /// Runs a single command on the DataStore.
-fn run_command(command: &str, store: &mut DataStore){
-
+fn run_command(command: &str, store: &mut DataStore) {
     let tokens: Vec<&str> = command.split(' ').collect::<Vec<&str>>();
 
     match tokens[0].to_uppercase().as_str() {
-        "GET" => {
-            if tokens.len() == 2 {
-                let out: Option<&Types> = store.get(tokens[1]);
-
-                print!("{}: ", tokens[1]);
-
-                types::print_data(out);
-                
-            }  else {
-                println!("Invalid arguments!")
-            }
-        },
+        // Strings
         "SET" => {
             if tokens.len() >= 3 {
-                store.set(tokens[1], Types::Text(tokens[2..tokens.len()].join(" ")));
-
+                store.set(tokens[1], Types::String(tokens[2..tokens.len()].join(" ")));
             } else {
                 println!("Not enough arguments!");
             }
-        },
-        _ =>{
+        }
+        "GET" => {
+            if tokens.len() == 2 {
+                match store.get(tokens[1]) {
+                    Some(data) => {
+                        print!("{}: ", tokens[1]);
+
+                        types::print_data(data.clone());
+                    }
+                    None => {
+                        print!("null");
+                    }
+                }
+            } else {
+                println!("Invalid arguments!")
+            }
+        }
+        "MGET" => {}
+        // Lists
+        "LPUSH" => {}
+        "LPOP" => {}
+        "RPUSH" => {}
+        "RPOP" => {}
+        "LLEN" => {}
+        "LMOVE" => {}
+        "LTRIM" => {}
+        // HashSets
+        "SADD" => {}
+        "SREM" => {}
+        "SISMEMBER" => {}
+        "SINTER" => {}
+        "SCARD" => {}
+        // HashMaps
+        "HSET" => {}
+        "HGET" => {}
+        "HMGET" => {}
+        "HINCRBY" => {}
+        // SortedSets
+        "ZADD" => {}
+        "ZRANGE" => {}
+        "ZRANK" => {}
+        "ZREVRANK" => {}
+        _ => {
             println!("Could not parse token \"{}\"", tokens[0]);
         }
     }
--- src/types/types.rs
+++ src/types/types.rs
@@ -1,71 +1,46 @@
+use std::collections::BTreeSet;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
 /// An enum that holds the allowed types.
 #[derive(Clone)]
 pub enum Types {
-    Text(String),
-    List(Vec<Option<Types>>),
-    Json(Vec<Option<KeyValue>>)
+    String(String),
+    List(Vec<String>),
+    Set(HashSet<String>),
+    Hash(HashMap<String, String>),
+    SortedSet(BTreeSet<String>),
 }
 
 /// A struct for a key value of a String and a Type.
 #[derive(Clone)]
 pub struct KeyValue {
     pub key: String,
-    pub value: Option<Types>
-}
-
-
-/// Converts a String to a Type
-/// 
-/// TODO: Not implemeted yet
-fn string_to_json(text: String) -> Option<Types> {
-    return Option::None;
+    pub value: Option<Types>,
 }
 
 /// Prints the given type.
-/// 
+///
 /// It does require the Type to be in an Option enum to allow for null types.
-pub fn print_data(data: Option<&Types>){
+pub fn print_data(data: Types) {
     match data {
-        Some(i) => {
-            match i {
-                Types::Text(out) => {
-                    print!("\"{}\"", out);
-                },
-                Types::List(out) => {
-                    print!("[ ");
+        Types::String(out) => {
+            print!("\"{}\"", out);
+        }
+        Types::List(out) => {
+            print!("[ ");
 
-                    for i in 0..out.len(){
+            for i in 0..out.len() {
+                print!("{}", out[i].as_str());
 
-                        print_data(out[i].as_ref());
-
-                        if i != out.len() - 1 {
-                            print!(", ");
-                        }
-                    }
-                    print!(" ]");
-                },
-                Types::Json(data) => {
-                    print!("{{ ");
-
-                    for i in 0..data.len(){
-                        match &data[i] {
-                            Some(j) => {
-                                print!("{}: ", j.key);
-
-                                print_data(j.value.as_ref())
-                            },
-                            None => {
-                                print!("null");
-                            }
-                        }
-                    }
-                    print!(" }}");
+                if i != out.len() - 1 {
+                    print!(", ");
                 }
             }
-        },
-        None =>{
-            println!("null");
+            print!(" ]");
         }
+        Types::Set(out) => {}
+        Types::Hash(out) => {}
+        Types::SortedSet(out) => {}
     }
 }
