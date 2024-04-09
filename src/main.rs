9af6adabc6f19b3624effddec09b87ea61ffe919
CCStudios <50129568+CodeCrashStudios@users.noreply.github.com>
2024-04-09 09:02:10 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,5 @@ use datastore::DataStore;
 fn main() {
     let mut data_store: DataStore = DataStore::new();
 
-    command_parser::parse("SET joe:1 this is a test;GET joe:1;", &mut data_store);
-
+    command_parser::parse("SET joe:1 this is a test; GET joe:1;", &mut data_store);
 }
